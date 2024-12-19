f94d0cf963318bc15f9cf126546d27c8ec336be8
Dean Wraith <hxr30eiw@students.codeinstitute.net>
2024-12-19 10:05:39 +0000

    Added secret key

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qzcz(!c^hess79&5t+p7^p$ay%0vv7hy%9&m3pd@zyv9^*l=4@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
